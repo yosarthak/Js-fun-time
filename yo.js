4562050eadf564dfe98f16717536412fc7314530
Sarthak <yosarthakyay@gmail.com>
2018-10-17 03:01:16 +0530

    Original hello world file

---
+++ yo.js
@@ -0,0 +1,10 @@
+var check = require('express');
+var appnew = check();
+
+appnew.get('/csi/', function (req, res){
+	res.send('Namit whats the count');
+});
+
+appnew.listen(6969, function() {
+	console.log('Hello Namit Ultra Chutiya CSI, running on port 6969 I believe');
+});
